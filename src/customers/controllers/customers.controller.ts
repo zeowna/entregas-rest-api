1b0607a36723ece4e1922dabd4fb13f0bbf59198
Zé Lucas Chociai <joselucaschociai@gmail.com>
2023-12-07 16:11:46 -0300

    fix

--- src/customers/controllers/customers.controller.ts
+++ src/customers/controllers/customers.controller.ts
@@ -7,9 +7,11 @@ import {
   Post,
   Query,
   Req,
+  UploadedFile,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
-import { CustomRequest } from '../../common';
+import { CustomRequest, ID } from '../../common';
 import { FindUsersService } from '../../users/services/find-users.service';
 import { FindUserByIdService } from '../../users/services/find-user-by-id.service';
 import { CreateUserService } from '../../users/services/create-user.service';
@@ -23,6 +25,9 @@ import { Roles } from '../../auth/decorators/roles.decorator';
 import { UserTypes } from '../../users/entities/user-types.enum';
 import { AuthGuard } from '../../common/auth';
 import { RolesGuard } from '../../auth/guards/routes.guard';
+import { ApiResponse } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadUserProfilePictureService } from 'src/users/services/upload-user-profile-picture.service';
 
 @Controller('customers')
 export class CustomersController {
@@ -31,6 +36,7 @@ export class CustomersController {
     private readonly findUserById: FindUserByIdService,
     private readonly createUserService: CreateUserService,
     private readonly updateUserService: UpdateUserService,
+    private readonly uploadUserProfilePictureService: UploadUserProfilePictureService,
   ) {}
 
   @Roles([UserTypes.Admin])
@@ -79,6 +85,23 @@ export class CustomersController {
       request.user.sub || +id,
       updateUserDto,
       request?.correlationId,
+    );
+  }
+
+  @ApiResponse({ type: String })
+  @Roles([UserTypes.Customer])
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post(':id([0-9]+)/pictures')
+  async uploadFile(
+    @Req() request: CustomRequest,
+    @Param('id') id: ID,
+    @UploadedFile('file') file: Express.Multer.File,
+  ) {
+    return this.uploadUserProfilePictureService.execute(
+      +id,
+      file,
+      request.correlationId,
     );
   }
 }
